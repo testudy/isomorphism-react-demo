c6e5594a75d2fe63809303afc3635d486e009f15
GuYue <guyue.jiwei@gmail.com>
2016-05-18 15:02:39 +0800

    开始material-ui

--- backend/static/js/app.js
+++ backend/static/js/app.js
@@ -1,1 +1,26 @@
-document.write('Hello Backend!!!');
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+
+import React, {
+    Component,
+} from 'react';
+import ReactDOM from 'react-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {
+    RaisedButton,
+} from 'material-ui';
+
+class App extends Component {
+
+    render() {
+        return (<MuiThemeProvider muiTheme={getMuiTheme()}>
+                <RaisedButton label="Default" />
+            </MuiThemeProvider>);
+    }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- backend/view/index.jsx
+++ backend/view/index.jsx
@@ -12,7 +12,7 @@ export default class Index extends Component {
 
         return (
             <Layout title={this.props.title}>
-                <h1>{this.props.title}</h1>
+                <div id="app" />
                 <script src="admin/vendors.js"></script>
                 <script src="admin/app.js"></script>
             </Layout>
