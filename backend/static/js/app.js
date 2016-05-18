660290e6cee3cdb87ab5d22f0416546b41474e3e
GuYue <guyue.jiwei@gmail.com>
2016-05-18 22:16:34 +0800

    test AppBar

--- backend/static/js/app.js
+++ backend/static/js/app.js
@@ -9,16 +9,32 @@ import React, {
 import ReactDOM from 'react-dom';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {
-    RaisedButton,
-} from 'material-ui';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 
 class App extends Component {
 
     render() {
-        return (<MuiThemeProvider muiTheme={getMuiTheme()}>
-                <RaisedButton label="Default" />
-            </MuiThemeProvider>);
+        return (
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <AppBar
+                    title="宝宝树评测系统"
+                    onTitleTouchTap={this.handleTouchTap}
+                    iconElementRight={<FlatButton label="OK" />}
+                    onLeftIconButtonTouchTap={this.handleRightIconButtonTouchTap}
+                />
+            </MuiThemeProvider>
+        );
+    }
+
+    handleTouchTap(e) {
+        console.log(e);
+        alert(e.type);
+    }
+
+    handleRightIconButtonTouchTap(e) {
+        console.log(e);
+        alert(e.type);
     }
 
 }
