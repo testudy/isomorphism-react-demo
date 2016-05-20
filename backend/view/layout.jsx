d77f990b7a7f8e2414fcec1ac5e4b1767d6f8bea
GuYue <guyue.jiwei@gmail.com>
2016-05-20 17:53:26 +0800

    模板添加基本样式

--- backend/view/layout.jsx
+++ backend/view/layout.jsx
@@ -15,6 +15,13 @@ const Layout = React.createClass({
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
                     <meta name="renderer" content="webkit" />
                     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui" />
+                    <style>{"\
+                        html { font-family: Roboto, sans-serif; -webkit-font-smoothing: antialiased; }\
+                        body, h1, h2, h3, h4, h5, h6 { margin: 0; }\
+                        body { font-size: 15px; line-height: 24px; }\
+                        a { color: #ff4081; text-decoration: none; }\
+                        a:hover { text-decoration: underline; }\
+                    "}</style>
                 </head>
                 <body>
                     {this.props.children}
