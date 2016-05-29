4eb3123e77ca7818bbe8ede94eb6b8eb66a88b3a
GuYue <guyue.jiwei@gmail.com>
2016-05-29 18:34:14 +0800

    refactor app router

--- frontend/common/AppRouter.jsx
+++ frontend/common/AppRouter.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {
+    Component,
+} from 'react';
+
 import {
+    Router,
     Route,
 } from 'react-router';
+
 import Index from './container/index.jsx';
 
+export default class AppRouter extends Component {
 
-export default [
-    <Route path="/" component={Index} key="index" />,
-    <Route path="/:date/:phone" component={Index} key="testing" />,
-];
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router {...this.props}>
+                <Route path="/" component={Index} />
+                <Route path="/:date/:phone" component={Index} />
+            </Router>
+        );
+    }
+}
