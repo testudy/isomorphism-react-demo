07c87c3e827926239dd194139f1220639b343d64
GuYue <guyue.jiwei@gmail.com>
2016-05-29 17:47:11 +0800

    修改语法错误

--- frontend/common/routes.jsx
+++ frontend/common/routes.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
-    Route
+    Route,
 } from 'react-router';
 import Index from './container/index.jsx';
 
 
 export default [
     <Route path="/" component={Index} key="index" />,
-    <Route path="/:date/:phone" component={Index} key="testing" />
+    <Route path="/:date/:phone" component={Index} key="testing" />,
 ];
