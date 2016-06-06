b6ccd8e11c4e3cca3681db7372da43b6ace09f80
GuYue <guyue.jiwei@gmail.com>
2016-06-06 21:16:33 +0800

    初步添加行为

--- frontend/common/container/index.jsx
+++ frontend/common/container/index.jsx
@@ -21,6 +21,83 @@ import style from '../style';
 
 class Index extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            nameErrorText: '',
+            phone: '',
+            phoneErrorText: '',
+        };
+    }
+
+
+    handleName(name) {
+        this.setState({
+            name: name.trim(),
+        });
+    }
+
+
+    validateName() {
+        const NAME_RE = /^[\w\u4e00-\u9fff]{2,}$/;
+
+        const name = this.state.name;
+
+        if (NAME_RE.test(name)) {
+            this.setState({
+                nameErrorText: '',
+            });
+            return true;
+        }
+
+        this.setState({
+            nameErrorText: '请填写您的姓名，方便我们后续和您联系',
+        });
+        return false;
+    }
+
+
+    handlePhone(phone) {
+        this.setState({
+            phone: phone.trim(),
+        });
+    }
+
+
+    validatePhone() {
+        const PHONE_RE = /^\d{3}\-?\d{4}\-?\d{4}$/;
+        const phone = this.state.phone;
+
+        if (PHONE_RE.test(phone)) {
+            this.setState({
+                phoneErrorText: '',
+            });
+            return true;
+        }
+
+        this.setState({
+            phoneErrorText: '请填写您的手机号码，方便我们后续和您联系',
+        });
+        return false;
+    }
+
+
+    submit() {
+        if (this.validateName() &&
+                this.validatePhone()) {
+            console.log('dispatch action');
+        }
+    }
+
+
+    handleKeyDown(keyCode) {
+        if (keyCode === 13) {
+            this.submit();
+        }
+    }
+
+
     render() {
         return (
             <MuiThemeProvider muiTheme={getMuiTheme()}>
@@ -42,14 +119,20 @@ class Index extends Component {
                             <CardText style={style.cardText}>
                                 <TextField
                                     hintText="王少伟"
-                                    errorText="请填写您的姓名，方便我们后续和您联系"
-                                    floatingLabelText="您的姓名"
+                                    errorText={this.state.nameErrorText}
+                                    floatingLabelText="请输入您的姓名"
+                                    defaultValue={this.state.name}
+                                    onChange={(event) => this.handleName(event.target.value)}
+                                    onKeyDown={(event) => this.handleKeyDown(event.keyCode)}
                                 />
                                 <br />
                                 <TextField
                                     hintText="186-1051-9100"
-                                    errorText="请填写您的手机号码，方便我们后续和您联系"
+                                    errorText={this.state.phoneErrorText}
                                     floatingLabelText="您的手机号码"
+                                    defaultValue={this.state.phone}
+                                    onChange={(event) => this.handlePhone(event.target.value)}
+                                    onKeyDown={(event) => this.handleKeyDown(event.keyCode)}
                                 />
                             </CardText>
                             <CardActions style={style.cardActions}>
@@ -61,6 +144,7 @@ class Index extends Component {
                                 }}
                                 primary={true}
                                 icon={<ContentSend />}
+                                onClick={(event) => this.submit()}
                             />
                             </CardActions>
                         </Card>
