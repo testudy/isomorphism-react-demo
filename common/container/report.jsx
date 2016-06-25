6167653eb5ab45cfa2e6f4dc99d530b4d5b64b6e
GuYue <guyue.jiwei@gmail.com>
2016-06-26 01:04:12 +0800

    调整样式

--- common/container/report.jsx
+++ common/container/report.jsx
@@ -36,67 +36,78 @@ class Report extends Component {
     render() {
         return (
             <div style={style.container}>
-            <DatePicker
-                hintText="根据日期查询评测报告"
-                container="inline"
-                mode="landscape"
-                disableYearSelection={true}
-                autoOk={true}
-                value={this.state.date}
-                onChange={(e, date) => {
-                    this.handleChange(date);
-                }}
-            />
-            <Table>
-                <TableHeader
-                    displaySelectAll={false}
-                >
-                    <TableRow>
-                        <TableHeaderColumn>序号</TableHeaderColumn>
-                        <TableHeaderColumn>姓名</TableHeaderColumn>
-                        <TableHeaderColumn>手机</TableHeaderColumn>
-                        <TableHeaderColumn>分数</TableHeaderColumn>
-                        <TableHeaderColumn>日期</TableHeaderColumn>
-                        <TableHeaderColumn>操作</TableHeaderColumn>
-                    </TableRow>
-                </TableHeader>
-                <TableFooter>
-                    <TableRow>
-                        <TableRowColumn style={{
-                            textAlign: 'right',
-                        }}>共100份</TableRowColumn>
-                    </TableRow>
-                </TableFooter>
-                <TableBody
-                    displayRowCheckbox={false}
-                    stripedRows={true}
-                >
-                    <TableRow>
-                        <TableRowColumn>1</TableRowColumn>
-                        <TableRowColumn>克念</TableRowColumn>
-                        <TableRowColumn>18610519156</TableRowColumn>
-                        <TableRowColumn>100</TableRowColumn>
-                        <TableRowColumn>2016.6.1</TableRowColumn>
-                        <TableRowColumn>详细</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableRowColumn>2</TableRowColumn>
-                        <TableRowColumn>克璟</TableRowColumn>
-                        <TableRowColumn>18610519157</TableRowColumn>
-                        <TableRowColumn>100</TableRowColumn>
-                        <TableRowColumn>2016.6.2</TableRowColumn>
-                        <TableRowColumn>详细</TableRowColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableRowColumn>3</TableRowColumn>
-                        <TableRowColumn>克勤</TableRowColumn>
-                        <TableRowColumn>18610519158</TableRowColumn>
-                        <TableRowColumn>100</TableRowColumn>
-                        <TableRowColumn>2016.6.3</TableRowColumn>
-                        <TableRowColumn>详细</TableRowColumn>
-                    </TableRow>
-                </TableBody>
-            </Table>
+                <h2 style={{
+                    textAlign: 'center',
+                }}>
+                    <DatePicker
+                        hintText="根据日期查询评测报告"
+                        container="inline"
+                        mode="landscape"
+                        disableYearSelection={true}
+                        autoOk={true}
+                        value={this.state.date}
+                        onChange={(e, date) => {
+                            this.handleChange(date);
+                        }}
+                        style={{
+                            display: 'inline-block',
+                        }}
+                        textFieldStyle={{
+                            width: '96px',
+                        }}
+                    />
+                    测评报告
+                </h2>
+                <Table>
+                    <TableHeader
+                        displaySelectAll={false}
+                    >
+                        <TableRow>
+                            <TableHeaderColumn>序号</TableHeaderColumn>
+                            <TableHeaderColumn>姓名</TableHeaderColumn>
+                            <TableHeaderColumn>手机</TableHeaderColumn>
+                            <TableHeaderColumn>分数</TableHeaderColumn>
+                            <TableHeaderColumn>日期</TableHeaderColumn>
+                            <TableHeaderColumn>操作</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableFooter>
+                        <TableRow>
+                            <TableRowColumn style={{
+                                textAlign: 'right',
+                            }}>共100份</TableRowColumn>
+                        </TableRow>
+                    </TableFooter>
+                    <TableBody
+                        displayRowCheckbox={false}
+                        stripedRows={true}
+                    >
+                        <TableRow>
+                            <TableRowColumn>1</TableRowColumn>
+                            <TableRowColumn>克念</TableRowColumn>
+                            <TableRowColumn>18610519156</TableRowColumn>
+                            <TableRowColumn>100</TableRowColumn>
+                            <TableRowColumn>2016.6.1</TableRowColumn>
+                            <TableRowColumn>详细</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>2</TableRowColumn>
+                            <TableRowColumn>克璟</TableRowColumn>
+                            <TableRowColumn>18610519157</TableRowColumn>
+                            <TableRowColumn>100</TableRowColumn>
+                            <TableRowColumn>2016.6.2</TableRowColumn>
+                            <TableRowColumn>详细</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>3</TableRowColumn>
+                            <TableRowColumn>克勤</TableRowColumn>
+                            <TableRowColumn>18610519158</TableRowColumn>
+                            <TableRowColumn>100</TableRowColumn>
+                            <TableRowColumn>2016.6.3</TableRowColumn>
+                            <TableRowColumn>详细</TableRowColumn>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </div>
         );
     }
